4747e534bbcd7abcb49fc9bb37981f2c3622e2cf
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-04-07 20:48:38 -0600

    added some dives and hover effect, now we have an beta version

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+function changePixelColor(e){
+    console.log('runing changePixelColor',e);
+
+    //pending
+    //we need a function that chanche the backgroun collor;
+
+}
+//helper functios upwards;
+const pictureContainer = document.getElementById('picture-container');
+pictureContainer.addEventListener('mouseover',changePixelColor);//now this works
+
+
+
+
+
+console.log(pictureContainer);
