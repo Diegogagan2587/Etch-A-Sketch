3115ffb5690ce1adf9fb1177f3075f4fb184f229
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-04-08 23:46:53 -0600

    Finnished Drawing Area Feature

--- js/main.js
+++ js/main.js
@@ -1,17 +1,46 @@
+function setSideSizeDrawingArea(newSize,pictureContainer){
+    pictureContainer.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
+    pictureContainer.style.gridTemplateRows = `repeat(${newSize},1fr)`;
+}
+function appenChildNumTimes(father,child,howManyTimes){
+    for(i = 0; i<howManyTimes; i++){
+        child.setAttribute("id",`pixel${i}`);//solve this but/////////////
+        father.appendChild(child.cloneNode(true));
+    }
+    return father;
+};
 
 function changePixelColor(e){
-    console.log('runing changePixelColor',e);
-
-    //pending
-    //we need a function that chanche the backgroun collor;
-
+    //e.cancelBubble = true;
+    console.log('runing changePixelColor','e',')');
+    let currentPixel = e.target;
+    currentPixel.setAttribute('class', 'blackPixel');
+}
+function reStartDrawing() {
+    let newSize = prompt('Please define a new length for all the sides?');
+    if(newSize>100){newSize = 100;};
+    pixelsPerSide = newSize;
+    prevNumOfElem = pictureContainer.childElementCount;
+    for(let i = 0; i<prevNumOfElem; i++){
+        pictureContainer.removeChild(pictureContainer.lastChild);
+    }
+    setSideSizeDrawingArea(newSize,pictureContainer);
+    appenChildNumTimes(pictureContainer,pixelDiv,newSize*newSize);
 }
 //helper functios upwards;
+
 const pictureContainer = document.getElementById('picture-container');
 pictureContainer.addEventListener('mouseover',changePixelColor);//now this works
-
-
-
-
-
 console.log(pictureContainer);
+//creatin the drawing Area;
+const pixelDiv = document.createElement('div');
+let pixelsPerSide = 16;
+pixelDiv.className = 'pixelFromDrawing'
+pixelDiv.innerText = ''
+pixelDiv.addEventListener('mouseover',changePixelColor);
+appenChildNumTimes(pictureContainer,pixelDiv,pixelsPerSide*pixelsPerSide);
+
+//Reset Button Functionaliti
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', reStartDrawing)
